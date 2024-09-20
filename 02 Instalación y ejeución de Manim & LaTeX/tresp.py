86b69dc0e1da8384071e60925c238758b64bbb2d
Diego GTG <diegogomeztagle51@gmail.com>
2024-09-19 22:53:23 -0600

    Se agregan los ultimos temas vistos en clase, graficacion de vectores 03,04 y 05

--- 02 Instalación y ejeución de Manim & LaTeX/tresp.py
+++ 02 Instalación y ejeución de Manim & LaTeX/tresp.py
@@ -1,9 +1,8 @@
 from manim import *  # Importa todas las clases y funciones de Manim
 
-class expresion(Scene):  # Define una escena de animación
+class formula_general(Scene):  # Define una escena de animación
     def construct(self):  # Método principal donde se construye la animación
-        text = MathTex("x^2")  # Crea una fórmula matemática "x^2"
+        text = MathTex("x=-b \pm \\frac{\sqrt{b^2-4ac}}{2ac}")  # Crea una fórmula matemática "x^2"
         self.add(text)  # Añade la fórmula a la escena
 
-#Para ejecutar, poner en consola
-#manim DosM.py expresion -pg
+#manim tresp.py formula_general -pg
