8b1c544c6d13b69a73f8f54489c46855d017eda7
DIEGO GOMEZ TAGLE GONZALEZ <77954404+DiegoGomezTagleG01@users.noreply.github.com>
2024-12-02 19:41:47 -0600

    Delete 02 Instalación y ejeución de Manim & LaTeX directory

--- 02 Instalación y ejeución de Manim & LaTeX/dosm.py
+++
@@ -1,9 +0,0 @@
-from manim import *  # Importa todas las clases y funciones de Manim
-
-class expresion(Scene):  # Define una escena de animación
-    def construct(self):  # Método principal donde se construye la animación
-        text = MathTex("x^2")  # Crea una fórmula matemática "x^2"
-        self.add(text)  # Añade la fórmula a la escena
-
-#Para ejecutar, poner en consola
-#manim DosM.py expresion -pg
--- 02 Instalación y ejeución de Manim & LaTeX/tresp.py
+++
@@ -1,8 +0,0 @@
-from manim import *  # Importa todas las clases y funciones de Manim
-
-class formula_general(Scene):  # Define una escena de animación
-    def construct(self):  # Método principal donde se construye la animación
-        text = MathTex("x=-b \pm \\frac{\sqrt{b^2-4ac}}{2ac}")  # Crea una fórmula matemática "x^2"
-        self.add(text)  # Añade la fórmula a la escena
-
-#manim tresp.py formula_general -pg
